39cc7e6fdf0c2849b72514d0caf686075751048a
chirag396rathod <ahirchirag396@gmail.com>
2023-09-13 23:06:33 +0530

    Complete fix issue

--- Backend/controllers/Chat/config.js
+++ Backend/controllers/Chat/config.js
@@ -5,9 +5,7 @@ export const ConnectSocketIO = () => {
   io.on("connection", (socket) => {
     console.log(`A user connected ${socket.id}`);
     socket.on("addUser", (userId) => {
-      const isUserExist = users.find(
-        (user) => user.socketId === socket.id || user.userId === userId
-      );
+      const isUserExist = users.find((user) => user.userId === userId);
 
       if (!isUserExist) {
         const user = { userId, socketId: socket?.id };
