aea4320f418772a877cedaabf9a32022acf69ae7
chirag396rathod <ahirchirag396@gmail.com>
2023-09-13 21:57:21 +0530

    log frontend console

--- Frontend/src/Utils/SocketClient.jsx
+++ Frontend/src/Utils/SocketClient.jsx
@@ -14,14 +14,22 @@ const SocketClient = () => {
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
-    const socketConnection = io(
-      import.meta.env.VITE_BASE_API_URL || "http://localhost:8080"
-    );
+    const socketConnection = io(import.meta.env.VITE_BASE_API_URL);
     setSocket(socketConnection);
     dispatch(handleSetSocket(socketConnection));
+    if (socketConnection.isConnected) {
+      console.log("Socket is connected!");
+    } else {
+      console.log("Socket is not connected!");
+    }
   }, []);
 
   useEffect(() => {
+    if (socket.isConnected) {
+      console.log("Socket is connected!");
+    } else {
+      console.log("Socket is not connected!");
+    }
     socket?.emit("addUser", USER_ID);
     socket?.on("getUser", (users) => {
       if (users) {
